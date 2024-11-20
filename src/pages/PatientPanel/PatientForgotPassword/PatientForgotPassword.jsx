91778c49ad8a6f9511a5841878a39c462494ea37
dhairydobariya <dhairydobariya@gmail.com>
2024-11-21 00:58:53 +0530

    login chage

--- src/pages/PatientPanel/PatientForgotPassword/PatientForgotPassword.jsx
+++ src/pages/PatientPanel/PatientForgotPassword/PatientForgotPassword.jsx
@@ -62,7 +62,7 @@ const PatientForgotPassword = () => {
                       Get OTP
                     </button>
                     <div className="text-center">
-                      <Link to={"/patient-login"} className="main-link mt-3">
+                      <Link to={"/"} className="main-link mt-3">
                         Back to Login
                       </Link>
                     </div>
